d735c0089ef1aab04e7ed231cd92357667bbdb33
Carlos Zillig <carloszilligg@gmail.com>
2022-08-28 12:08:46 -0300

    refatoração do codigo js e css

--- Home page/assets/js/menu-mobile.js
+++ Home page/assets/js/menu-mobile.js
@@ -1,14 +1,22 @@
 const menu = document.querySelector(".menu-mobile")
 const menuOpenBtn = document.querySelector(".header-menu--btn");
-const menuCloseBtn = document.querySelector(".menu-close")
+const menuCloseBtn = document.querySelector(".menu-close");
+const menuLinks = document.querySelectorAll(".header-link")
 
 menuOpenBtn.addEventListener("click", () => {
     toggleMenu(menu)
-})
+});
 
 menuCloseBtn.addEventListener("click", () => {
     toggleMenu(menu)
+});
+
+menuLinks.forEach(item => {
+    item.addEventListener("click", _ => {
+        toggleMenu(menu)
+    })
 })
+
 
 function toggleMenu(element) {
     element.classList.toggle("active-menu")
--- Home page/assets/js/questions.js
+++ Home page/assets/js/questions.js
@@ -1,26 +1,33 @@
-const questionsBtn = document.querySelectorAll(".questions-content");
+const questionsBtn = document.querySelectorAll(".questions-item");
 
 questionsBtn.forEach(item => {
-    const textBox = document.querySelectorAll(".questions-text");
+    const textBox = document.querySelectorAll(".questions-text--container");
     item.addEventListener('click', function (event) {
         let target = event.target;
+        //tentei usar Operações ternarias mas ficaria muito feio o codigo
         if (target.id == 1) {
+            //t1 = first textBox
+            //i1 = first arrow icon
             let box = textBox.id = t1;
-            box.classList.toggle("display-none")
             let icon = document.querySelector("#i1");
-            icon.classList.toggle("rotate")
+            toggleClass(box, "display-none")
+            toggleClass(icon, "rotate")
         }
         if (target.id == 2) {
             let box = textBox.id = t2;
-            box.classList.toggle("display-none")
             let icon = document.querySelector("#i2");
-            icon.classList.toggle("rotate")
+            toggleClass(box, "display-none")     
+            toggleClass(icon, "rotate")
         }
         if (target.id == 3) {
             let box = textBox.id = t3;
-            box.classList.toggle("display-none")
             let icon = document.querySelector("#i3");
-            icon.classList.toggle("rotate")
+            toggleClass(box, "display-none")
+            toggleClass(icon, "rotate")
         }
     })
 })
+
+function toggleClass (element, id) {
+    element.classList.toggle(id)
+}
